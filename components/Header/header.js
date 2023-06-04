e6bc217bbe766eedea0b1e3b3ba07b4e813bdd47
B.L.S. Thathsara <65841165+BLSThathsara20@users.noreply.github.com>
2023-06-04 18:16:26 +0530

    first commit

--- components/Header/header.js
+++ components/Header/header.js
@@ -1,33 +1,64 @@
 import React from 'react';
 import styles from '@/components/Header/Header.module.scss'
+import Image from 'next/image';
 
 const Header = () => (
   <header className={styles.header}>
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
-        <div className={styles.headerWrap}>
-          <div className={styles.logoImg}>
-            <img src="" alt="" />
-          </div>
-          <nav className={styles.nav}>
-            <ul>
-              <li><a href="#">SERVICES</a></li>
-              <li><a href="#">SUBSCRIPTIONS</a></li>
-              <li><a href="#">CONTACT US</a></li>
-              <li><a href="#">BLOGS</a></li>
-            </ul>
-          </nav>
-          <div>
-            
-          </div>
-          <div >
-            <a href="#">SIGN IN</a>
-          </div>
-          <div >
-            <a href="#">JOIN</a>
+        <a className="navbar-brand" href="/">
+          <Image className='img-fluid' src="/home/vairal-logo.png" width={142} height={26}/>
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className={`${styles.headerNavlink} nav-link active text-uppercase`} aria-current="page" href="#">
+              SERVICES
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className={`${styles.headerNavlink} nav-link text-uppercase`} href="#">
+              SUBSCRIPTIONS
+              </a>
+            </li>
+            <li  className="nav-item">
+              <a className={`${styles.headerNavlink} nav-link text-uppercase`} href="#">
+              ABOUT US
+              </a>
+            </li>
+            <li  className="nav-item">
+              <a className={`${styles.headerNavlink} nav-link text-uppercase`} href="#">
+              CONTACT US
+              </a>
+            </li>
+            <li  className="nav-item">
+              <a className={`${styles.headerNavlink} nav-link text-uppercase`} href="#">
+              BLOGS
+              </a>
+            </li>
+          </ul>
+          <div className="d-flex">
+            <button className="btn btn-outline-primary me-2 fw-bold fs-6" type="button">
+              SIGIN IN
+            </button>
+            <button className="btn btn-primary fw-bold fs-6" type="button">
+              JOIN
+            </button>
           </div>
         </div>
       </div>
-
+    </nav>
   </header>
 );
 
